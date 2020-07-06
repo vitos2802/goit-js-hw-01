efbc34775a5eb2f077835bd7868c22618453dd21
Viktor Zayats <viktor_zayats@ukr.net>
2020-07-06 19:39:41 +0300

    no coments

--- js/task-06.js
+++ js/task-06.js
@@ -4,7 +4,6 @@ let total = 0;
 
 do {
   input = prompt("Введите число");
-  // const notAnumber = Number.isNaN(input);
   if (isNaN(Number(input))) {
     alert("Было введено не число, попробуйте еще раз");
     continue;
